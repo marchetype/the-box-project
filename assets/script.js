680f12edd0fddf57393cc382aca5485997cc8df8
devitodev <softwaredev.ito@gmail.com>
2022-12-06 19:02:40 -0600

    added a few different changes: 1) added an animation box 2) pulled an api for getting jokes 3) reorganized the html to make it more aesthetically appealing

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,77 @@
+// How to open the modal
+$(document).ready(function (){
+  $('#jokebox').modal()
+})
+
 $(document).ready(function(){
-    $('#openedbox').modal();
-  });
+  $('#dogebox').modal();
+});
+
+
+function getaJoke() {
+      var jokeAPI = 'https://official-joke-api.appspot.com/random_joke'
+      fetch(jokeAPI)
+      .then(function (response){
+          return response.json()
+      })
+      .then(function (data) {
+          console.log(data)
+          renderJoke(data);
+      });
+  }
+
+getaJoke()
+
+function renderJoke(data) {
+  $('#setup').text(data.setup);
+  $('#punchline').text(data.punchline);
+}
+
+// refresh button function
+// var getRefreshBtn = $('#refresh-btn')
+// getRefreshBtn.click(function() {
+//     location.reload();
+// });
+
+// function getaDoge () {
+//     var dogeAPI = 'http://shibe.online/api/shibes?count=[1-100]&urls=true&httpsUrls=true'
+//     fetch(dogeAPI)
+//     .then(function (response){
+//         return response.json()
+//     })
+//     .then(function (data2) {
+//         console.log(data2)
+//         renderDoge(data2)
+//     });
+// }
+
+// getaDoge()
+
+// function renderDoge (data2) {
+//     $('#dogetime').attr('src', data2[0])
+// }
+
+
+
+// Grabbing the inside value of the div (hopefully)
+// var relatedArtistSpot = $('#artist-spot').val();
+// var coldplay = '4gzpq5DPGxSnKTe4SA8HAU'
+
+
+
+
+
+
+// Failed attempt at: Function to pull related artists API from Spoftify (hopefully again)
+// function getRelatedArtists() {
+//     var relatedArtists = `https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU/related-artists`
+//     fetch(relatedArtists)
+//     .then(function (response){
+//         return response.json()
+//     })
+//     .then(function (data) {
+      
+//     });
+// }
+
+// getRelatedArtists()
